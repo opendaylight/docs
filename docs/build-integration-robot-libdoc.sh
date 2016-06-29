199205046fd007c6888f34ec6968a0f8ba744535
Jamo Luhrsen <jluhrsen@redhat.com>
2016-06-29 16:29:20 -0700

    make html prettier for robot libdocs
    Change-Id: Iba8814c0dcf8addb2a46a5148cb727d94c614086
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- docs/build-integration-robot-libdoc.sh
+++ docs/build-integration-robot-libdoc.sh
@@ -6,4 +6,5 @@ mkdir -p $ROBOT_DIR
 cd submodules/integration/test/csit/libraries
 for f in *.robot; do python -m robot.libdoc $f $ROBOT_DIR/$f.html; done
 for f in *.py; do python -m robot.libdoc $f $ROBOT_DIR/$f.html; done
-
+cd $ROBOT_DIR/
+for f in *.html; do sed -i 's/black/orange/; s/white/black/; s/^<body>/<body><img src="..\/..\/odl.png">/' $f; done
