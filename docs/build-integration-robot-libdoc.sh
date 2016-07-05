23aabf3dd2e70c30f8271e7af6e2391a33823b14
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-07-05 16:55:26 -0400

    Redirect expected robot html generation failures
    Send robot html generation failures to /dev/null as they are expected
    and can confuse those who might be building the project.
    
    Make sed on OS X happy by creating robot htmls in $TMP_ROBOT_DIR and
    redirecting sed to $ROBOT_DIR for final copy.
    
    Change-Id: If38694b1865696d3ae7ec7f6f6e7e1e6cd618ed8
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs/build-integration-robot-libdoc.sh
+++ docs/build-integration-robot-libdoc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ROBOT_DIR=`pwd`/_static/integration/robot
+TMP_ROBOT_DIR=`pwd`/_build/robot
 
-mkdir -p $ROBOT_DIR
+mkdir -p $ROBOT_DIR $TMP_ROBOT_DIR
 cd submodules/integration/test/csit/libraries
-for f in *.robot; do python -m robot.libdoc $f $ROBOT_DIR/$f.html; done
-for f in *.py; do python -m robot.libdoc $f $ROBOT_DIR/$f.html; done
-cd $ROBOT_DIR/
-for f in *.html; do sed -i 's/black/orange/; s/white/black/; s/^<body>/<body><img src="..\/..\/odl.png">/' $f; done
+for f in *.robot; do python -m robot.libdoc $f $TMP_ROBOT_DIR/$f.html; done
+for f in *.py; do python -m robot.libdoc $f $TMP_ROBOT_DIR/$f.html > /dev/null 2>&1; done
+cd $TMP_ROBOT_DIR/
+for f in *.html; do sed 's/black/orange/; s/white/black/; s/^<body>/<body><img src="..\/..\/odl.png">/' $f > $ROBOT_DIR/$f; done
