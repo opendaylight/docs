6486cb78e0d97db6e41022e563fd48e7e11fbde1
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-07-05 16:55:26 -0400

    Redirect expected robot html generation failures
    Send robot html generation failures to /dev/null as they are expected
    and can confuse those who might be building the project.
    
    Also adding '' as an option to -i for sed to make sed on OS X happy.
    
    Change-Id: If38694b1865696d3ae7ec7f6f6e7e1e6cd618ed8
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs/build-integration-robot-libdoc.sh
+++ docs/build-integration-robot-libdoc.sh
@@ -5,6 +5,6 @@ ROBOT_DIR=`pwd`/_static/integration/robot
 mkdir -p $ROBOT_DIR
 cd submodules/integration/test/csit/libraries
 for f in *.robot; do python -m robot.libdoc $f $ROBOT_DIR/$f.html; done
-for f in *.py; do python -m robot.libdoc $f $ROBOT_DIR/$f.html; done
+for f in *.py; do python -m robot.libdoc $f $ROBOT_DIR/$f.html > /dev/null 2>&1; done
 cd $ROBOT_DIR/
-for f in *.html; do sed -i 's/black/orange/; s/white/black/; s/^<body>/<body><img src="..\/..\/odl.png">/' $f; done
+for f in *.html; do sed -i '' 's/black/orange/; s/white/black/; s/^<body>/<body><img src="..\/..\/odl.png">/' $f; done
