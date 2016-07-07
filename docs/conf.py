ba754dceb86a282fc57e53185419dbc668bc84ee
An Ha <zero579911@gmail.com>
2016-07-07 15:24:46 -0400

    Enable javasphinx
    This patch allows us to generate java api doc using sphinx.
    
    Change-Id: I2e7eeeede1c5fa667a191b625beca175f3324682
    Signed-off-by: An Ha <zero579911@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ import sphinx_bootstrap_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['javasphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
