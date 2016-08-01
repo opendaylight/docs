07ea1f0a82dd3043e015e3350d02212eaae3923a
Xinghao Gu <brian.gu1991@gmail.com>
2016-08-01 18:47:54 -0500

    Split whole index.rst(developer-guide,user-guide)
    The scripts generates developer-guide and user-guide reST files and build to html with sphinx.
    Still there're bugs especially for links and figures in the reSTs need to be fixed.
    
    Change-Id: I45cfe68b0e88cf0ea87e1512bff21a11662c6b08
    Signed-off-by: Xinghao Gu <brian.gu1991@gmail.com>

--- makeRST.sh
+++ makeRST.sh
@@ -4,24 +4,28 @@
 # converting  asciidoc to reStructedText automatically using
 # asciidoctor maven plug in and pandoc (http://www.pandoc.org/).
 #
-# Please make sure you have pandoc (http://www.pandoc.org/) and sphinx
-# (http://www.sphinx-doc.org/) installed.
+# Please make sure you have pandoc (http://www.pandoc.org/), sphinx
+# (http://www.sphinx-doc.org/) and sphinx-bootstrap-theme(https://ryan-roemer.github.io/sphinx-bootstrap-theme/) installed.
 
 cwd=$(pwd)
 cp -r $cwd/docs/getting-started-guide $cwd/docs_autotranslation/getting-started-guide
+cp -r $cwd/docs/opendaylight-with-openstack $cwd/docs_autotranslation/opendaylight-with-openstack
 cp -r $cwd/docs/_static $cwd/docs_autotranslation/_static
-docs='howto-openstack developer-guide user-guide'
+docs='developer-guide user-guide'
 for i in $docs
- do
-	echo "Translating $i ..."
-	cd $cwd/manuals/$i
-	mvn clean compile
-	cp -r $cwd/manuals/$i/target/generated-docs $cwd/docs_autotranslation/$i
-	mvn clean
-	cd $cwd/docs_autotranslation/$i
-	pandoc -f docbook -t rst -s *.xml -o index.rst
- done
+do
+    echo "Translating $i ..."
+    cd $cwd/manuals/$i
+    mvn clean compile
+    cp -r $cwd/manuals/$i/target/generated-docs $cwd/docs_autotranslation/$i
+    mvn clean
+    cd $cwd/docs_autotranslation/$i
+    pandoc -f docbook -t rst -s *.xml -o index.rst
+done
 
+cd $cwd/docs_autotranslation
+./split.py -i developer-guide/index.rst
+./split.py -i user-guide/index.rst
 
 sphinx-build -b html $cwd/docs_autotranslation $cwd/docs_autotranslation/html
 
