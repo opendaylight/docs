f18047490ca847a0a5ed392cb2dc778be0cd6027
Colin Dixon <colin@colindixon.com>
2016-08-12 16:20:28 -0400

    Get spectrometer autodoc working
    Change-Id: I66b9c834392ebb937262b9799d83f076cd88464b
    Co-Authored-By: Thanh Ha <thanh.ha@linuxfoundation.org>
    Signed-off-by: Colin Dixon <colin@colindixon.com>

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import sphinx_bootstrap_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('submodules/spectrometer/server'))
 
 # -- General configuration ------------------------------------------------
 
@@ -30,7 +30,7 @@ import sphinx_bootstrap_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 # Disable javasphinx generation until we have a solution to long build
 # times. readthedocs timesout after 902 seconds.
