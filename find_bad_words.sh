01758cae1c3558069317ed85aaf8734c0e89a298
Colin Dixon <colin@colindixon.com>
2016-09-06 17:05:50 -0400

    Update find_bad_words.sh
    Change-Id: I7e80bae2b3a61acc953e29a94dde89cf9a79637e
    Signed-off-by: Colin Dixon <colin@colindixon.com>

--- find_bad_words.sh
+++ find_bad_words.sh
@@ -1,3 +1,6 @@
-egrep --color -nir --include=*.{adoc,rst} beryllium\|lithium .
-egrep --color -nir --include=*.{adoc,rst} open\ ?flow . | grep -v OpenFlow | grep -v openflow: | grep -v \-openflow\- | grep -v openflowplugin | grep -v openflowjava | grep -v Openflow13 | grep -v \_OPENFLOW | grep -v OpenflowNode | grep --color -i OpenFlow
-egrep --color -nir --include=*.{adoc,rst} open\ ?daylight . | grep -v OpenDaylight | grep -v \.opendaylight\. | grep -v \/opendaylight | grep -v \=opendaylight | grep --color -i OpenDaylight
+egrep --color -nir --include=*.rst beryllium\|lithium docs
+egrep --color -nir --include=*.rst open\ ?flow docs | grep -v OpenFlow | grep -v openflow: | grep -v \-openflow\- | grep -v openflowplugin | grep -v openflowjava | grep -v Openflow13 | grep -v \_OPENFLOW | grep -v OpenflowNode | grep --color -i OpenFlow
+egrep --color -nir --include=*.rst open\ ?daylight docs | grep -v OpenDaylight | grep -v \.opendaylight\. | grep -v \/opendaylight | grep -v \=opendaylight | grep --color -i OpenDaylight
+egrep --color -nir --include=*.rst [^-]acl[^-_\":] docs | grep -v ACL | grep -vi maclearn | grep -vi oracle | grep --color -i acl
+#the ovs[db] search seemed to produce only false positives
+#egrep --color -nir --include=*.rst [^-/_\"\']ovs[^-:k_] docs | grep -v OVS | egrep -v ovsdb[:-] | grep --color -i ovs
