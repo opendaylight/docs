ce514b96610e8c4987a80e6ae7d5eebc5019ecad
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-11-09 10:15:06 -0500

    Remove javasphinx
    The community has spoken and decided that we will continue generating
    javadoc using javadoc. We now have javadocs.opendaylight.org to handle
    this.
    
    Change-Id: I5ef2aac6e8a4e6f1bafcdde663fb9ceda4d44fd5
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -32,16 +32,6 @@ sys.path.insert(0, os.path.abspath('submodules/spectrometer/server'))
 # ones.
 extensions = ['sphinx.ext.autodoc','sphinx.ext.graphviz']
 
-# Disable javasphinx generation until we have a solution to long build
-# times. readthedocs timesout after 902 seconds.
-javasphinx_available = False
-try:
-    import javasphinx
-    javasphinx_available = True
-    extensions.append('javasphinx')
-except ImportError, e:
-    pass
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -324,9 +314,3 @@ if platform.system() != 'Windows':
     import subprocess
 
     subprocess.call(["./build-integration-robot-libdoc.sh"])
-
-    # Disable javasphinx generation until we have a solution to long build
-    # times. readthedocs timesout after 902 seconds.
-    if javasphinx_available:
-        subprocess.call(["./generate-javaapidoc.sh"])
-
