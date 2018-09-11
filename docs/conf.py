0cc9f076fa000dcb22d0161017e0a0494b0949f0
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-09-10 22:32:02 -0400

    Add intersphinx for aaa and remove them from docs
    AAA is now available at https://docs.opendaylight.org/projects/aaa
    so remove them from local docs, we can link to them instead.
    
    Issue: DOCS-69
    Change-Id: Ifd1f7f0e6d6a5af546e8c04f203ce9ed5da7b2dd
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ intersphinx_mapping['odl-integration-packaging'] = ('https://docs.opendaylight.o
 intersphinx_mapping['odl-releng-builder'] = ('https://docs.opendaylight.org/projects/releng-builder/en/latest/', None)
 
 # Projects that have stable/branches
+intersphinx_mapping['aaa'] = ('https://docs.opendaylight.org/projects/aaa/en/stable-fluorine/', None)
 intersphinx_mapping['coe'] = ('https://docs.opendaylight.org/projects/coe/en/stable-fluorine/', None)
 intersphinx_mapping['genius'] = ('https://docs.opendaylight.org/projects/genius/en/stable-fluorine/', None)
 intersphinx_mapping['infrautils'] = ('https://docs.opendaylight.org/projects/infrautils/en/stable-fluorine/', None)
