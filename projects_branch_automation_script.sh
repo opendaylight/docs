25f0757162ec713ca3332254c40ced6c929c31f9
Anushka Bhandari <anushka16134@iiitd.ac.in>
2019-08-04 00:05:59 +0530

    Automation script for branching and updating in projects
    Automate the creation of stable/<release> branch in the listed ODL projects.
    The master also points to <release> in the docs/conf.yaml.
    
    Signed-off-by: Anushka Bhandari <anushka16134@iiitd.ac.in>
    Change-Id: I83adef6b5cdbcd9e9baeff4c0a8bcc587fd95d9a

---
+++ projects_branch_automation_script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+'''
+./projects_branch_automation_script.sh -d path_to_ODL_projects -l sodium
+'''
+while getopts ":d:l:" opt; do
+  case $opt in
+    d)
+      dir=$OPTARG
+      echo "-d directory path : $OPTARG" >&2;;
+    l)
+      latest_release=$OPTARG
+      echo "-l latest_release : $OPTARG" >&2;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+ODL_DIR=$dir
+master=$latest_release
+branch="stable/"$master
+
+# Captilize Version Name
+release_version="$(tr '[:lower:]' '[:upper:]' <<< "${master:0:1}")${master:1}"
+mapfile -t odl_projects < <(grep -oP '(?<=module>)[^<]+' "$ODL_DIR""/autorelease/pom.xml")
+
+for i in ${!odl_projects[*]}
+do
+    odl_project=${odl_projects[$i]##*/}
+    path_project=$ODL_DIR"/${odl_project}"
+    git checkout master
+    cd "$path_project" || exit
+    sed -i "s/.*version.*/version: $release_version/" docs/conf.yaml
+    git checkout -b "$branch" master # create new branch using master as base
+    git checkout "$branch"
+    cat docs/conf.yaml
+done
+
