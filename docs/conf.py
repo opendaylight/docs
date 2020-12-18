b9e76a2fdd0595428e4144431d44791615f68b20
Guillaume Lambert <guillaume.lambert@orange.com>
2020-12-18 14:09:59 +0100

    migrate coding guidelines from the old wiki
    Signed-off-by: Guillaume Lambert <guillaume.lambert@orange.com>
    Change-Id: I72bcd0fd21cc902bf1b97b5cd3f32c063536376c

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,7 @@ linkcheck_ignore = [
     'https://jenkins.opendaylight.org/sandbox',
     # The '#' in the path makes sphinx think it's an anchor
     'https://git.opendaylight.org/gerrit/#/admin/projects/releng/builder',
+    'https://git.opendaylight.org/gerrit/#/c/',
     'https://git.opendaylight.org/gerrit/gitweb',
     # URL returns a 403 Forbidden
     'https://www.osgi.org',
