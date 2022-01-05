7f4dcef38ca23fd1c2c6ac7d0a11803ee350e982
guillaume.lambert <guillaume.lambert@orange.com>
2022-01-05 17:44:06 +0100

    Fix conf.py objects.inv references
    -remove genius and opflex from the list
    -use stable-phosphorus in MSI projects ref
    -use the nearest version available in RTD in MSI objects.inv ref
    
    Issue: IT-23601
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: If9222e0d539055180316b78d7dfc559db76843ed

--- docs/conf.py
+++ docs/conf.py
@@ -14,29 +14,34 @@ from docs_conf.conf import *
 import sys, os
 
 # Append to intersphinx_mapping
-intersphinx_mapping['aaa'] = ('https://docs.opendaylight.org/projects/aaa/en/latest/', None)
-intersphinx_mapping['controller'] = ('https://docs.opendaylight.org/projects/controller/en/latest/', None)
-intersphinx_mapping['infrautils'] = ('https://docs.opendaylight.org/projects/infrautils/en/latest/', None)
-intersphinx_mapping['mdsal'] = ('https://docs.opendaylight.org/projects/mdsal/en/latest/', None)
-intersphinx_mapping['netconf'] = ('https://docs.opendaylight.org/projects/netconf/en/latest/', None)
+# for P-SR2 MRI version can be found at https://git.opendaylight.org/gerrit/c/integration/distribution/+/99752
+intersphinx_mapping['aaa'] = ('https://docs.opendaylight.org/projects/aaa/en/v0.14.8/', None)
+# should be v0.14.10 but this version is not listed as available in RTD
+intersphinx_mapping['bgpcep'] = ('https://docs.opendaylight.org/projects/bgpcep/en/v0.16.13/', None)
+# should be v0.16.14 but this version is not listed as available in RTD
+intersphinx_mapping['controller'] = ('https://docs.opendaylight.org/projects/controller/en/v4.0.3/', None)
+# should be v4.0.10
+intersphinx_mapping['infrautils'] = ('https://docs.opendaylight.org/projects/infrautils/en/v2.0.6/', None)
+# should be v2.0.13
+intersphinx_mapping['mdsal'] = ('https://docs.opendaylight.org/projects/mdsal/en/v8.0.5/', None)
+# should be v8.0.11
+intersphinx_mapping['netconf'] = ('https://docs.opendaylight.org/projects/netconf/en/v2.0.12/', None)
+# should be v2.0.14
 intersphinx_mapping['odl-integration-test'] = ('https://docs.opendaylight.org/projects/integration-test/en/latest/', None)
 intersphinx_mapping['odl-integration-packaging'] = ('https://docs.opendaylight.org/projects/integration-packaging/en/latest/', None)
 intersphinx_mapping['odl-releng-builder'] = ('https://docs.opendaylight.org/projects/releng-builder/en/latest/', None)
-intersphinx_mapping['opflex'] = ('https://docs.opendaylight.org/projects/opflex/en/latest/', None)
 
 # Projects that have stable/branches
-intersphinx_mapping['bgpcep'] = ('https://docs.opendaylight.org/projects/bgpcep/en/latest/', None)
-intersphinx_mapping['daexim'] = ('https://docs.opendaylight.org/projects/daexim/en/latest/', None)
-intersphinx_mapping['genius'] = ('https://docs.opendaylight.org/projects/genius/en/latest/', None)
-intersphinx_mapping['integration-distribution'] = ('https://docs.opendaylight.org/projects/integration-distribution/en/latest/', None)
-intersphinx_mapping['jsonrpc'] = ('https://docs.opendaylight.org/projects/jsonrpc/en/latest/', None)
-intersphinx_mapping['lispflowmapping'] = ('https://docs.opendaylight.org/projects/lispflowmapping/en/latest/', None)
-intersphinx_mapping['openflowplugin'] = ('https://docs.opendaylight.org/projects/openflowplugin/en/latest/', None)
-intersphinx_mapping['ovsdb'] = ('https://docs.opendaylight.org/projects/ovsdb/en/latest/', None)
-intersphinx_mapping['transportpce'] = ('https://docs.opendaylight.org/projects/transportpce/en/latest/', None)
+intersphinx_mapping['daexim'] = ('https://docs.opendaylight.org/projects/daexim/en/stable-phosphorus/', None)
+intersphinx_mapping['integration-distribution'] = ('https://docs.opendaylight.org/projects/integration-distribution/en/stable-phosphorus/', None)
+intersphinx_mapping['jsonrpc'] = ('https://docs.opendaylight.org/projects/jsonrpc/en/stable-phosphorus/', None)
+intersphinx_mapping['lispflowmapping'] = ('https://docs.opendaylight.org/projects/lispflowmapping/en/stable-phosphorus/', None)
+intersphinx_mapping['openflowplugin'] = ('https://docs.opendaylight.org/projects/openflowplugin/en/stable-phosphorus/', None)
+intersphinx_mapping['ovsdb'] = ('https://docs.opendaylight.org/projects/ovsdb/en/stable-phosphorus/', None)
+intersphinx_mapping['transportpce'] = ('https://docs.opendaylight.org/projects/transportpce/en/stable-phosphorus/', None)
 
 # OpenDaylight Documentation Releases
-intersphinx_mapping['odl-phosphorus'] = ('https://docs.opendaylight.org/en/latest/', None)
+intersphinx_mapping['odl-phosphorus'] = ('https://docs.opendaylight.org/en/stable-phosphorus/', None)
 intersphinx_mapping['odl-silicon'] = ('https://docs.opendaylight.org/en/stable-silicon/', None)
 intersphinx_mapping['odl-aluminium'] = ('https://docs.opendaylight.org/en/stable-aluminium/', None)
 intersphinx_mapping['odl-magnesium'] = ('https://docs.opendaylight.org/en/stable-magnesium/', None)
