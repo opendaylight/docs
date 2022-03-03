4a3bbcd64a7d4f1567c80f18a4867f725e3a7cea
guillaume.lambert <guillaume.lambert@orange.com>
2022-03-03 21:02:43 +0100

    Improve RTD projects branches activation
    Some projects branches need to be manually activated by docs maintainers
    with a script and a RTD personnal token.
    This is especially true for MRI projects
    Though, the current script only allows MSI projects activation.
    
    - remove MRI projects from the old script mostly targeting MSI projects
    - create a MRI projects versions list in TSV format
    - copy/paste old script and adapt it quickly and dirtily
      to activate and build MRI projects branches
    
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: Ia9fa2c61c012970398d204196d5be78925374c34

--- activate-mri-projects-rtd-branch.sh
+++ activate-mri-projects-rtd-branch.sh
@@ -16,10 +16,9 @@
 
 
 usage() {
-    echo "Usage: $0 <token> <version_name>"
+    echo "Usage: $0 <token>"
     echo ""
     echo "    token:  RTD API Token"
-    echo "    version_name:  Name of the version to be activated e.g stable-aluminium."
     echo ""
 }
 
@@ -47,6 +46,18 @@ update_available_versions() {
         -H "Content-Length: 0"
 }
 
+# Build  to force RTD to update available versions
+build_version() {
+    token=$1
+    project_name=$2
+    version_name=$3
+
+    echo "Forcing RTD to update available versions"
+    curl -X POST "https://readthedocs.org/api/v3/projects/$project_name/versions/$version_name/builds/" \
+        -H "Authorization: Token $token" \
+        -H "Content-Length: 0"
+}
+
 while getopts :h: opts; do
     case "$opts" in
         h)
@@ -60,7 +71,7 @@ while getopts :h: opts; do
     esac
 done
 
-if [ -z $2 ]; then
+if [ -z $1 ]; then
     usage
     exit 1
 fi
@@ -70,20 +81,9 @@ fi
 ################
 
 token="$1"
-version_name="$2"
 
-supported_projects=(
-    odl-daexim
-    opendaylight-distribution
-    odl-jsonrpc
-    odl-lispflowmapping
-    odl-openflowplugin
-    opendaylight-ovsdb
-    odl-transportpce
-    )
-
-for project in ${supported_projects[@]}; do
-    update_available_versions "$token" "$project" "$version_name"
+for project in $(cut -f1 < MRI_projects_to_activate.list); do
+    update_available_versions "$token" "odl-$project"
 done
 
 echo ""
@@ -91,6 +91,8 @@ echo ""
 echo "Waiting 60 seconds for available versions to update"
 sleep 60  # Wait a minute for RTD to update available versions
 
-for project in ${supported_projects[@]}; do
-    activate_version "$token" "$project" "$version_name"
+for project in $(cut -f1 < MRI_projects_to_activate.list); do
+    version_name=$(grep $project MRI_projects_to_activate.list | cut -f2)
+    activate_version "$token" "odl-$project" "$version_name"
+    build_version "$token" "odl-$project" "$version_name"
 done
