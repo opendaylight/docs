fc7424f078ec7db2c36a309f026a384e76b1b789
jenkins-releng <jenkins-releng@opendaylight.org>
2024-06-17 23:29:27 +0000

    Update release notes for Potassium-SR3
    Install and run enchant conditionally on Ubuntu or CentOS.
    This is required for moving the builders to Ubuntu.
    
    Change-Id: I3433bdcd6f5edb0d540852246cdb27f7f507b0ff
    Signed-off-by: jenkins-releng <jenkins-releng@opendaylight.org>
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ install-enchant.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -x
+
+os="$(lsb_release -is)"
+
+if [[ "${os}" =~ "Ubuntu" ]]; then
+    command enchant>/dev/null || command enchant-2 -v>/dev/null || \
+    sudo apt-get update && sudo apt-get --yes --force-yes install enchant-2
+elif [[ "${os}" =~ "CentOS" ]]; then
+    command enchant>/dev/null || command enchant-2 -v>/dev/null || \
+    sudo yum -y install enchant
+else
+    echo "enchant command not found on ${os}. Please install and try again"
+    exit 1
+fi
