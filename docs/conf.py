8f07c6d82591c271d6581326d5564097f814f0d9
jenkins-releng <jenkins-releng@opendaylight.org>
2024-06-17 23:29:27 +0000

    Update release notes for Potassium-SR3
    Change-Id: I3433bdcd6f5edb0d540852246cdb27f7f507b0ff
    Signed-off-by: jenkins-releng <jenkins-releng@opendaylight.org>
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ def update_intersphinx_mapping():
 update_intersphinx_mapping()
 
 # OpenDaylight Documentation Releases
+intersphinx_mapping['odl-calcium'] = ('https://docs.opendaylight.org/en/latest/', None)
 intersphinx_mapping['odl-potassium'] = ('https://docs.opendaylight.org/en/latest/', None)
 intersphinx_mapping['odl-argon'] = ('https://docs.opendaylight.org/en/stable-argon/', None)
 intersphinx_mapping['odl-chlorine'] = ('https://docs.opendaylight.org/en/stable-chlorine/', None)
