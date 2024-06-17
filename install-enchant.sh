4ee7d0f722aad4ea6935d9753aa53ac9105873c5
jenkins-releng <jenkins-releng@opendaylight.org>
2024-06-17 23:29:27 +0000

    Update release notes for Potassium-SR3
    Install and run enchant on Ubuntu.
    
    Change-Id: I3433bdcd6f5edb0d540852246cdb27f7f507b0ff
    Signed-off-by: jenkins-releng <jenkins-releng@opendaylight.org>
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ install-enchant.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -x
+
+os="$(lsb_release -is)"
+if [ "$os" == "Ubuntu" ]; then
+  sudo apt-get update && sudo apt-get --yes --force-yes install enchant-2
+elif [ "$os" == "CentOS" ]; then
+  sudo yum -y install enchant
+else
+  echo "enchant command not found - please install"
+fi
