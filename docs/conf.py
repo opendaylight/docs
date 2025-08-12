4f621782e0764e6db25702e0aa634061bb636294
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2025-08-12 08:55:42 +0200

    Fix docs link-check configuration
    - add missing , for each ignored URI line
    - add https://www.opendaylight.org/current-release-titanium to the list
    
    Change-Id: I1a92159d7c5fa9723e117acc9df319359a621c6a
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    (cherry picked from commit 0573e0d07b0c02ac49a067af1f2f45a09b922696)

--- docs/conf.py
+++ docs/conf.py
@@ -76,10 +76,11 @@ linkcheck_ignore = [
     # this url often has connectivity issues on the gate
     'https://www.mojohaus.org/animal-sniffer/animal-sniffer-maven-plugin/examples/checking-signatures.html',
     # this url often has connectivity issues on the gate
-    'https://cwiki.apache.org/confluence/display/PEKKO/Pekko+Akka+Compatibility'
+    'https://cwiki.apache.org/confluence/display/PEKKO/Pekko+Akka+Compatibility',
     # this url often has connectivity issues on the gate
-    'https://wiki-archive.opendaylight.org/images/c/ca/Profile-Java-ODL.xml.zip'
+    'https://wiki-archive.opendaylight.org/images/c/ca/Profile-Java-ODL.xml.zip',
     #Ignore announce page link till it is up
+    'https://www.opendaylight.org/current-release-titanium',
     'https://www.opendaylight.org/current-release-scandium',
     'https://www.opendaylight.org/current-release-calcium',
 ]
